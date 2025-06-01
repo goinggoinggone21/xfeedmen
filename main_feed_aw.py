b531d1633dfaff854ec6ebe07da2ce1ae0319f5c
goinggoinggone21 <goinggoinggone21@proton.me>
2025-06-01 13:44:31 -0700

    Update main_feed_aw.py

--- main_feed_aw.py
+++ main_feed_aw.py
@@ -18,20 +18,20 @@ import helper
 
 imojis_list = ['🤍','💟','😍','🤩','🫣','🥵','🥹','❤️‍🔥','🫶','👀','🫦','🔥','✨','💖','💦','⭐','👑']
 def get_tweet_title(reddit_title):
-    try:
-        reddit_title_clean = reddit_title.replace('[Discussion]','').replace('[Pick]','')
-        #print('reddit_title_clean ', reddit_title_clean)
-        brackets = reddit_title_clean.split('[')[1].split(']')[0]
-        #print('brackets ',brackets)
-        final_twitter_title = brackets.title() + ' ' + random.choice(imojis_list)
-        #print(final_twitter_title)
-        return final_twitter_title.replace("'S","'s")
-    except:
-        print('No Brackets')
-        final_twitter_title = random.choice(imojis_list)*3
-        #print(final_twitter_title)
-        return final_twitter_title
-        pass
+	try:
+		reddit_title_clean = reddit_title.replace('[Discussion]','').replace('[Pick]','')
+		#print('reddit_title_clean ', reddit_title_clean)
+		brackets = reddit_title_clean.split('[')[1].split(']')[0]
+		#print('brackets ',brackets)
+		final_twitter_title = brackets.title() + ' ' + random.choice(imojis_list)
+		#print(final_twitter_title)
+		return final_twitter_title.replace("'S","'s")
+	except:
+		print('No Brackets')
+		final_twitter_title = random.choice(imojis_list)*3
+		#print(final_twitter_title)
+		return final_twitter_title
+		pass
 
 
 #print('num of arguments: ', len(sys.argv))
@@ -40,11 +40,11 @@ def get_tweet_title(reddit_title):
 input_args = sys.argv
 
 reddit = praw.Reddit(client_id=str(input_args[1]), #REDDIT_CLIENT_ID
-					 client_secret=str(input_args[2]),#REDDIT_CLIENT_SECRET
-					 password=str(input_args[3]), #REDDIT_PASSWORD
-					 user_agent=str(input_args[4]), #REDDIT_USER_AGENT
-					 username=str(input_args[5]) #REDDIT_USER_NAME
-					 )
+						client_secret=str(input_args[2]),#REDDIT_CLIENT_SECRET
+						password=str(input_args[3]), #REDDIT_PASSWORD
+						user_agent=str(input_args[4]), #REDDIT_USER_AGENT
+						username=str(input_args[5]) #REDDIT_USER_NAME
+						)
 twitter_api_authorized = Api(
 		access_token=input_args[6], #TWITTER_ACCESS_TOKEN,
 		access_secret=input_args[7], #TWITTER_ACCESS_TOKEN_SECRET
@@ -61,7 +61,7 @@ try:
 		all_urls_ever = pickle.load(fp)
 		#print(todays_alreadysent_list)
 except:
-    print("Didn't find historical urls pickle file")
+				print("Didn't find historical urls pickle")
 	all_urls_ever = []
 
 
@@ -88,70 +88,70 @@ print('tweet_title_final: ', tweet_title_final)
 
 filename = 'to_upload.mp4'
 if os.path.exists(filename):
-    os.remove(filename)
+				os.remove(filename)
 
 try:
-    #while (not os.path.isfile(filename)) & (str(submission_url) not in all_urls_ever):
-    video_url = helper.get_redgifs_embedded_video_url(redgifs_url=submission_url, output_fn=filename)
-    total_bytes = os.path.getsize(filename)
-    print('total_bytes: ', total_bytes)
-    if int(total_bytes) < 1000000:
-        print('File Size Too Small')
-        with open('all_urls_ever.ob', 'wb') as fp:
-            #pickle.dump([], fp)
-            pickle.dump(all_urls_ever, fp)
-        #continue
-    resp = twitter_api_authorized.upload_media_chunked_init(
-        total_bytes=total_bytes,
-        media_type="video/mp4",
-    )
-    media_id = resp.media_id_string
-    #print(media_id)
+	#while (not os.path.isfile(filename)) & (str(submission_url) not in all_urls_ever):
+	video_url = helper.get_redgifs_embedded_video_url(redgifs_url=submission_url, output_fn=filename)
+	total_bytes = os.path.getsize(filename)
+	print('total_bytes: ', total_bytes)
+	if int(total_bytes) < 1000000:
+					print('File Size Too Small')
+					with open('all_urls_ever.ob', 'wb') as fp:
+									#pickle.dump([], fp)
+									pickle.dump(all_urls_ever, fp)
+					#continue
+	resp = twitter_api_authorized.upload_media_chunked_init(
+					total_bytes=total_bytes,
+					media_type="video/mp4",
+	)
+	media_id = resp.media_id_string
+	#print(media_id)
 
-    segment_id = 0
-    bytes_sent = 0
-    file = open(filename, 'rb')
-    idx=0
-    while bytes_sent < total_bytes:
-        chunk = file.read(4*1024*1024)
-        status = twitter_api_authorized.upload_media_chunked_append(
-                media_id=media_id,
-                media=chunk,
-                segment_index=idx
-            )
-        idx = idx+1
+	segment_id = 0
+	bytes_sent = 0
+	file = open(filename, 'rb')
+	idx=0
+	while bytes_sent < total_bytes:
+					chunk = file.read(4*1024*1024)
+					status = twitter_api_authorized.upload_media_chunked_append(
+													media_id=media_id,
+													media=chunk,
+													segment_index=idx
+									)
+					idx = idx+1
 
-        bytes_sent = file.tell()
-        #print(idx, media_id, status, bytes_sent)
+					bytes_sent = file.tell()
+					#print(idx, media_id, status, bytes_sent)
 
-    resp = twitter_api_authorized.upload_media_chunked_finalize(media_id=media_id)
-    print(resp)
+	resp = twitter_api_authorized.upload_media_chunked_finalize(media_id=media_id)
+	print(resp)
 
 
-    time.sleep(30)
-    resp = twitter_api_authorized.upload_media_chunked_status(media_id=media_id)
-    print(resp)
+	time.sleep(30)
+	resp = twitter_api_authorized.upload_media_chunked_status(media_id=media_id)
+	print(resp)
 
-    twitter_api_authorized.create_tweet(
-        media_media_ids=[media_id], 
-        text=tweet_title_final
-    )
+	twitter_api_authorized.create_tweet(
+					media_media_ids=[media_id], 
+					text=tweet_title_final
+	)
 
-    os.remove(filename)
+	os.remove(filename)
 
-    all_urls_ever.append(submission_url)
-    with open(all_urls_fn, 'wb') as fp:
-        #pickle.dump([], fp)
-        pickle.dump(all_urls_ever, fp)
-    if os.path.exists(filename):
-        os.remove(filename)
-    #print('pausing')
-    #time.sleep(600) #random.choice(range(7000))
+	all_urls_ever.append(submission_url)
+	with open(all_urls_fn, 'wb') as fp:
+					#pickle.dump([], fp)
+					pickle.dump(all_urls_ever, fp)
+	if os.path.exists(filename):
+					os.remove(filename)
+	#print('pausing')
+	#time.sleep(600) #random.choice(range(7000))
 except Exception:
-    print('error in flow')
-    print(traceback.format_exc())
-    pass
-    #continue
+	print('error in flow')
+	print(traceback.format_exc())
+	pass
+	#continue
 
 
 		
