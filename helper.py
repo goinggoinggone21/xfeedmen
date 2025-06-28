93a5b429559ec322a7650e04fe6b212d8e55301d
goinggoinggone21 <goinggoinggone21@proton.me>
2025-06-28 13:07:57 -0700

    Adding linux FFMPEG

--- helper.py
+++ helper.py
@@ -2,8 +2,8 @@ import requests
 import datetime
 import random
 import requests, traceback, json, io, os, urllib.request, sys
-import moviepy
-#from moviepy.editor import VideoFileClip
+#import moviepy
+from moviepy.editor import VideoFileClip
 
 #sys.stdout.reconfigure(encoding='utf-8')
 
@@ -24,7 +24,7 @@ def has_audio(filepath):
 
     try:
         # Load the video clip
-        clip = moviepy.editor.VideoFileClip(filepath)
+        clip = VideoFileClip(filepath)
 
         # Check if the audio attribute is not None
         if clip.audio is not None:
