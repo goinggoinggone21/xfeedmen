b1b820f8dfc01b6ab53079323dd0d0dd255a0f95
goinggoinggone21 <goinggoinggone21@proton.me>
2025-09-02 22:37:37 -0700

    Update main_feed_ma.py

--- main_feed_ma.py
+++ main_feed_ma.py
@@ -19,23 +19,23 @@ import helper
 imojis_list = ['🥰','😍','🤩','🫣','🥵','🥹','❤️‍🔥','🫶','👀','🫦','🔥','✨','💖']
 heart_imojis = ['❤️','💙','💚','💛','💜','💟','🖤','♥️','🧡','🤍','🤎','💦']
 def get_tweet_title(reddit_title):
-    try:
-        reddit_title_clean = reddit_title.replace('[Discussion]','').replace('[Pick]','')
-        if (('thelisaann' in reddit_title_clean.lower()) | 
-                ('comments' in reddit_title_clean.lower()) | (len(reddit_title_clean.lower()) < 4)):
-            reddit_title_clean = reddit_title_clean.replace('[','')
-        #print('reddit_title_clean ', reddit_title_clean)
-        brackets = reddit_title_clean.split('[')[1].split(']')[0]
-        #print('brackets ',brackets)
-        final_twitter_title = brackets.title() + ' ' + random.choice(imojis_list)
-        #print(final_twitter_title)
-        return final_twitter_title.replace("'S","'s")
-    except:
-        print('No Brackets')
-        final_twitter_title = random.choice(heart_imojis)*3
-        #print(final_twitter_title)
-        return final_twitter_title
-        pass
+	try:
+		reddit_title_clean = reddit_title.replace('[Discussion]','').replace('[Pick]','')
+		if (('thelisaann' in reddit_title_clean.lower()) | 
+				('comments' in reddit_title_clean.lower()) | (len(reddit_title_clean.lower()) < 4)):
+			reddit_title_clean = reddit_title_clean.replace('[','')
+		#print('reddit_title_clean ', reddit_title_clean)
+		brackets = reddit_title_clean.split('[')[1].split(']')[0]
+		#print('brackets ',brackets)
+		final_twitter_title = brackets.title() + ' ' + random.choice(imojis_list)
+		#print(final_twitter_title)
+		return final_twitter_title.replace("'S","'s")
+	except:
+		print('No Brackets')
+		final_twitter_title = random.choice(heart_imojis)*3
+		#print(final_twitter_title)
+		return final_twitter_title
+		pass
 
 
 #print('num of arguments: ', len(sys.argv))
@@ -68,11 +68,11 @@ except:
 
 bad_urls_fn = 'bad_urls_ma.ob'
 try:
-    with open (bad_urls_fn, 'rb') as fp:
-        bad_urls = pickle.load(fp)
-        #print(todays_alreadysent_list)
+	with open (bad_urls_fn, 'rb') as fp:
+		bad_urls = pickle.load(fp)
+		#print(todays_alreadysent_list)
 except:
-    bad_urls = []
+	bad_urls = []
 
 
 #Load Reddits
@@ -103,83 +103,78 @@ print('tweet_title_final_with_tags: ', tweet_title_final_with_tags)
 
 filename = 'to_upload_ma.mp4'
 if os.path.exists(filename):
-    os.remove(filename)
+	os.remove(filename)
 
 try:
-    #while (not os.path.isfile(filename)) & (str(submission_url) not in all_urls_ever):
-    video_url = helper.get_redgifs_embedded_video_url(redgifs_url=submission_url, output_fn=filename)
-    file_has_audio = helper.has_audio(filename)   
-    if not file_has_audio:
+	#while (not os.path.isfile(filename)) & (str(submission_url) not in all_urls_ever):
+	video_url = helper.get_redgifs_embedded_video_url(redgifs_url=submission_url, output_fn=filename)
+	file_has_audio = helper.has_audio(filename)   
+	if not file_has_audio:
 		print('no audio')
 		if os.path.exists(filename):
 			os.remove(filename)
-    if not os.path.exists(filename): #if url is bad or has no audio, add it to bad list
+	if not os.path.exists(filename): #if url is bad or has no audio, add it to bad list
 		print('file bad; adding to bads')
-        bad_urls.append(submission_url)
-        with open(bad_urls_fn, 'wb') as fp:
-            #pickle.dump([], fp)
-            pickle.dump(bad_urls, fp)
-    total_bytes = os.path.getsize(filename)
-    print('total_bytes: ', total_bytes)
-    if int(total_bytes) < 1000000:
-        print('File Size Too Small')
-        with open('all_urls_ever.ob', 'wb') as fp:
-            #pickle.dump([], fp)
-            pickle.dump(all_urls_ever, fp)
-        #continue
-    resp = twitter_api_authorized.upload_media_chunked_init(
-        total_bytes=total_bytes,
-        media_type="video/mp4",
-    )
-    media_id = resp.media_id_string
-    #print(media_id)
+		bad_urls.append(submission_url)
+		with open(bad_urls_fn, 'wb') as fp:
+			#pickle.dump([], fp)
+			pickle.dump(bad_urls, fp)
+	total_bytes = os.path.getsize(filename)
+	print('total_bytes: ', total_bytes)
+	if int(total_bytes) < 1000000:
+		print('File Size Too Small')
+		with open('all_urls_ever.ob', 'wb') as fp:
+			#pickle.dump([], fp)
+			pickle.dump(all_urls_ever, fp)
+		#continue
+	resp = twitter_api_authorized.upload_media_chunked_init(
+		total_bytes=total_bytes,
+		media_type="video/mp4",
+	)
+	media_id = resp.media_id_string
+	#print(media_id)
 
-    segment_id = 0
-    bytes_sent = 0
-    file = open(filename, 'rb')
-    idx=0
-    while bytes_sent < total_bytes:
-        chunk = file.read(4*1024*1024)
-        status = twitter_api_authorized.upload_media_chunked_append(
-                media_id=media_id,
-                media=chunk,
-                segment_index=idx
-            )
-        idx = idx+1
+	segment_id = 0
+	bytes_sent = 0
+	file = open(filename, 'rb')
+	idx=0
+	while bytes_sent < total_bytes:
+		chunk = file.read(4*1024*1024)
+		status = twitter_api_authorized.upload_media_chunked_append(
+				media_id=media_id,
+				media=chunk,
+				segment_index=idx
+			)
+		idx = idx+1
 
-        bytes_sent = file.tell()
-        #print(idx, media_id, status, bytes_sent)
+		bytes_sent = file.tell()
+		#print(idx, media_id, status, bytes_sent)
 
-    resp = twitter_api_authorized.upload_media_chunked_finalize(media_id=media_id)
-    print(resp)
+	resp = twitter_api_authorized.upload_media_chunked_finalize(media_id=media_id)
+	print(resp)
 
 
-    time.sleep(30)
-    resp = twitter_api_authorized.upload_media_chunked_status(media_id=media_id)
-    print(resp)
+	time.sleep(30)
+	resp = twitter_api_authorized.upload_media_chunked_status(media_id=media_id)
+	print(resp)
 
-    twitter_api_authorized.create_tweet(
-        media_media_ids=[media_id], 
-        text=tweet_title_final_with_tags
-    )
+	twitter_api_authorized.create_tweet(
+		media_media_ids=[media_id], 
+		text=tweet_title_final_with_tags
+	)
 
-    os.remove(filename)
+	os.remove(filename)
 
-    all_urls_ever.append(submission_url)
-    with open('all_urls_ever.ob', 'wb') as fp:
-        #pickle.dump([], fp)
-        pickle.dump(all_urls_ever, fp)
-    if os.path.exists(filename):
-        os.remove(filename)
-    #print('pausing')
-    #time.sleep(600) #random.choice(range(7000))
+	all_urls_ever.append(submission_url)
+	with open('all_urls_ever.ob', 'wb') as fp:
+		#pickle.dump([], fp)
+		pickle.dump(all_urls_ever, fp)
+	if os.path.exists(filename):
+		os.remove(filename)
+	#print('pausing')
+	#time.sleep(600) #random.choice(range(7000))
 except Exception:
-    print('error in flow')
-    print(traceback.format_exc())
-    pass
-    #continue
-
-
-		
-		
-
+	print('error in flow')
+	print(traceback.format_exc())
+	pass
+	#continue
